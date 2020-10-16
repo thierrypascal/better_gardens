24e98beb7ed669e57fd9a1a3114a269d43b09eda
Janko Lisica <janko.lisica@students.fhnw.ch>
2020-10-16 10:37:02 +0200

    added an account page and navigation using the drawer

--- lib/main.dart
+++ lib/main.dart
@@ -18,13 +18,13 @@ class MyApp extends StatelessWidget {
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Home Page'),
+      home: LoginPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class LoginPage extends StatefulWidget {
+  LoginPage({Key key}) : super(key: key);
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
@@ -35,14 +35,13 @@ class MyHomePage extends StatefulWidget {
   // used by the build method of the State. Fields in a Widget subclass are
   // always marked "final".
 
-  final String title;
+  final String title = 'Login';
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _LoginPageState createState() => _LoginPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-
+class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
@@ -60,60 +59,91 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       drawer: MyDrawer(),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-          child: Container (
-            padding: EdgeInsets.all(20),
-            child: Column(
-              // Column is also a layout widget. It takes a list of children and
-              // arranges them vertically. By default, it sizes itself to fit its
-              // children horizontally, and tries to be as tall as its parent.
-              //
-              // Invoke "debug painting" (press "p" in the console, choose the
-              // "Toggle Debug Paint" action from the Flutter Inspector in Android
-              // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-              // to see the wireframe for each widget.
-              //
-              // Column has various properties to control how it sizes itself and
-              // how it positions its children. Here we use mainAxisAlignment to
-              // center the children vertically; the main axis here is the vertical
-              // axis because Columns are vertical (the cross axis would be
-              // horizontal).
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: <Widget>[
-                Container(
-                  padding: EdgeInsets.fromLTRB(0, 0, 0, 120),
-                  child: Image.asset('res/logo.png',
-                    width: 150,),
-                ),
-                TextField(
-                  decoration: InputDecoration(
-                    labelText: 'Email',
-                    hintText: 'john.doe@mail.com',
-                  ),
-                ),
-                TextField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                      labelText: 'Password',
-                      hintText: 'Enter password'
-                  ),
-                ),
-                RaisedButton(
-                    child: Text('Login'),
-                    onPressed: null
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text('Sign-Up'),
-                    Text('Forgot Password?')
-                  ],
-                )
-              ],
+          // Center is a layout widget. It takes a single child and positions it
+          // in the middle of the parent.
+          child: Container(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug painting" (press "p" in the console, choose the
+          // "Toggle Debug Paint" action from the Flutter Inspector in Android
+          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+          // to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.fromLTRB(0, 0, 0, 120),
+              child: Image.asset(
+                'res/logo.png',
+                width: 150,
+              ),
             ),
-          )
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Email',
+                hintText: 'john.doe@mail.com',
+              ),
+            ),
+            TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                  labelText: 'Password', hintText: 'Enter password'),
+            ),
+            RaisedButton(child: Text('Login'), onPressed: null),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [Text('Sign-Up'), Text('Forgot Password?')],
+            )
+          ],
+        ),
+      )), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class AccountPage extends StatefulWidget {
+  AccountPage({Key key}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  @override
+  _AccountPageState createState() => _AccountPageState();
+}
+
+class _AccountPageState extends State<AccountPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Account'),
+      ),
+      drawer: MyDrawer(),
+      body: Container(
+        child: ListView(
+          children: [
+            UserAccountsDrawerHeader(
+              accountEmail: Text('markus.hofer@gmail.com'),
+              accountName: Text('Markus Hofer'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
@@ -121,13 +151,22 @@ class _MyHomePageState extends State<MyHomePage> {
 class MyDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return  Drawer(
+    return Drawer(
       child: ListView(
         // Important: Remove any padding from the ListView.
         padding: EdgeInsets.zero,
         children: <Widget>[
           DrawerHeader(
             child: Text('Drawer Header'),
+          ),
+          ListTile(
+            title: Text('Login'),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => LoginPage()),
+              );
+            },
           ),
           ListTile(
             title: Text('Map'),
@@ -138,8 +177,10 @@ class MyDrawer extends StatelessWidget {
           ListTile(
             title: Text('Account'),
             onTap: () {
-              // Update the state of the app.
-              // ...
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => AccountPage()),
+              );
             },
           ),
           ListTile(
