bcdaf509cbb14e5fde2c55780b6a3b5b9e2e3f3b
ThierryOdermatt <68233005+ThierryOdermatt@users.noreply.github.com>
2020-10-19 18:16:45 +0200

    fixed overflow on keyboard opened

--- lib/main.dart
+++ lib/main.dart
@@ -45,68 +45,53 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        //leading: Icon(Icons.menu),
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       drawer: MyDrawer(),
       body: Center(
-          // Center is a layout widget. It takes a single child and positions it
-          // in the middle of the parent.
-          child: Container(
-        padding: EdgeInsets.all(20),
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: <Widget>[
-            Container(
-              padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
-              child: Image.asset(
-                'res/logo.png',
-                width: 180,
+        child: LayoutBuilder(
+          builder: (BuildContext context, BoxConstraints viewportConstraints) {
+            return SingleChildScrollView(
+              padding: EdgeInsets.fromLTRB(30, 0, 30, 0),
+              child: ConstrainedBox(
+                constraints: BoxConstraints(
+                  minHeight: viewportConstraints.maxHeight,
+                ),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: <Widget>[
+                    Container(
+                      child: Image.asset(
+                        'res/logo.png',
+                        width: 180,
+                      ),
+                    ),
+                    TextField(
+                      decoration: InputDecoration(
+                        labelText: 'Email',
+                        hintText: 'john.doe@mail.com',
+                      ),
+                    ),
+                    TextField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                          labelText: 'Password', hintText: 'Enter password'),
+                    ),
+                    RaisedButton(child: Text('Login'), onPressed: null),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [Text('Sign-Up'), Text('Forgot Password?')],
+                    )
+                  ],
+                ),
               ),
-            ),
-            TextField(
-              decoration: InputDecoration(
-                labelText: 'Email',
-                hintText: 'john.doe@mail.com',
-              ),
-            ),
-            TextField(
-              obscureText: true,
-              decoration: InputDecoration(
-                  labelText: 'Password', hintText: 'Enter password'),
-            ),
-            RaisedButton(child: Text('Login'), onPressed: null),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [Text('Sign-Up'), Text('Forgot Password?')],
-            )
-          ],
+            );
+          },
         ),
-      )), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
