ce927657b122381bd6546037a2c950c478bf7a74
Janko Lisica <janko.lisica@students.fhnw.ch>
2020-10-21 15:22:55 +0200

    Made the sublists a stateful widget because they use the same code

--- lib/ListPage.dart
+++ lib/ListPage.dart
@@ -26,16 +26,16 @@ class _ListPageState extends State<ListPage> {
             bottom: TabBar(
               tabs: [
                 Tab(text: 'ELEMENT',),
-                Tab(text: 'PFLANZE',),
-                Tab(text: 'METHODE',),
+                Tab(text: 'PLANT',),
+                Tab(text: 'METHOD',),
               ],
             ),
           ),
           body: TabBarView(
             children: <Widget>[
-              ElementList(),
-              PlantList(),
-              MethodList(),
+              SubList(elementType: 'Element',),
+              SubList(elementType: 'Plant',),
+              SubList(elementType: 'Method',)
             ],
           ),
         ),
@@ -45,34 +45,16 @@ class _ListPageState extends State<ListPage> {
 }
 
 //This class needs to be replaced: load the elements from database and change each listelement into expandable
-class ElementList extends StatelessWidget {
-  ElementList({Key key}) : super(key: key);
+class SubList extends StatefulWidget {
+  SubList({Key key, this.elementType}) : super(key: key);
+
+  final String elementType;
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: CustomScrollView(
-        slivers: <Widget>[
-          SliverList(
-            // Use a delegate to build items as they're scrolled on screen.
-            delegate: SliverChildBuilderDelegate(
-              // The builder function returns a ListTile with a title that
-              // displays the index of the current item.
-                  (context, index) => ListTile(title: Text('Element #$index')),
-              // Builds 200 ListTiles
-              childCount: 200,
-            ),
-          ),
-        ],
-      ),
-    );
-  }
+  _SubListState createState() => _SubListState();
 }
 
-//This class needs to be replaced: load the elements from database and change each listelement into expandable
-class PlantList extends StatelessWidget {
-  PlantList({Key key}) : super(key: key);
-
+class _SubListState extends State<SubList> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -80,28 +62,7 @@ class PlantList extends StatelessWidget {
         slivers: <Widget>[
           SliverList(
             delegate: SliverChildBuilderDelegate(
-                  (context, index) => ListTile(title: Text('Pflanze #$index')),
-              childCount: 200,
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-//This class needs to be replaced: load the elements from database and change each listelement into expandable
-class MethodList extends StatelessWidget {
-  MethodList({Key key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: CustomScrollView(
-        slivers: <Widget>[
-          SliverList(
-            delegate: SliverChildBuilderDelegate(
-                  (context, index) => ListTile(title: Text('Methode #$index')),
+                  (context, index) => ListTile(title: Text( widget.elementType +' #$index')),
               childCount: 200,
             ),
           ),
