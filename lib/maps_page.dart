1687b7a7ea3da41ba81814d9a55e2be773d8b310
ThierryOdermatt <68233005+ThierryOdermatt@users.noreply.github.com>
2020-11-23 09:15:57 +0100

    create new branch addIcons

--- lib/maps_page.dart
+++ lib/maps_page.dart
@@ -1,5 +1,4 @@
 import 'dart:developer' as logging;
-
 import 'package:biodiversity/drawer.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
