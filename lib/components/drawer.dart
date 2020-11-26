57da2412e9c235cf3432cb1a1f5a16272a6aaa89
ThierryOdermatt <68233005+ThierryOdermatt@users.noreply.github.com>
2020-11-26 13:59:58 +0100

    small change to drawer

--- lib/components/drawer.dart
+++ lib/components/drawer.dart
@@ -9,68 +9,74 @@ class MyDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: ListView(
-        // Important: Remove any padding from the ListView.
-        padding: EdgeInsets.zero,
-        children: <Widget>[
-          const DrawerHeader(
-            child: Text(''),
-          ),
-          ListTile(
-            title: const Text('Login'),
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => LoginPage()),
-              );
-            },
-          ),
-          ListTile(
-            title: const Text('Map'),
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => MapsPage()),
-              );
-            },
-          ),
-          ListTile(
-            title: const Text('Account'),
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => AccountPage()),
-              );
-            },
-          ),
-          ListTile(
-            title: const Text('My Garden'),
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => MyGarden()),
-              );
-              // ...
-            },
-          ),
-          ListTile(
-            title: const Text('Inventar'),
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => InformationListPage()),
-              );
-            },
-          ),
-          ListTile(
-            title: const Text('Bookmarks'),
-            onTap: () {
-              // Update the state of the app.
-              // ...
-            },
-          ),
-        ],
-      ),
-    );
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              decoration: const BoxDecoration(
+                color: Colors.green,
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Image.asset('res/logo.png', fit: BoxFit.contain,),
+              ),
+            ),
+            ListTile(
+              title: const Text('Login'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => LoginPage()),
+                );
+              },
+            ),
+            ListTile(
+              title: const Text('Map'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => MapsPage()),
+                );
+              },
+            ),
+            ListTile(
+              title: const Text('Account'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => AccountPage()),
+                );
+              },
+            ),
+            ListTile(
+              title: const Text('My Garden'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => MyGarden()),
+                );
+                // ...
+              },
+            ),
+            ListTile(
+              title: const Text('Inventar'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => InformationListPage()),
+                );
+              },
+            ),
+            ListTile(
+              title: const Text('Bookmarks'),
+              onTap: () {
+                // Update the state of the app.
+                // ...
+              },
+            ),
+          ],
+        ),
+      );
   }
 }
