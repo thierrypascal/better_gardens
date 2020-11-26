5c848d3ad05344dc8f01f07a9de3a783525b9931
ThierryOdermatt <68233005+ThierryOdermatt@users.noreply.github.com>
2020-11-26 18:33:34 +0100

    add animated FAB on map

--- lib/screens/map_page/maps_page.dart
+++ lib/screens/map_page/maps_page.dart
@@ -8,19 +8,30 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:provider/provider.dart';
+import 'dart:math' as math;
 
 class MapsPage extends StatefulWidget {
   @override
   _MapsPageState createState() => _MapsPageState();
 }
 
-class _MapsPageState extends State<MapsPage> {
+class _MapsPageState extends State<MapsPage> with TickerProviderStateMixin {
   GoogleMapController mapController;
   LatLng _lastLocation;
+  AnimationController _controller;
+  static const List<IconData> icons = const [
+    Icons.dashboard_customize,
+    Icons.playlist_add,
+  ];
+
 
   @override
   void initState() {
     super.initState();
+    _controller = new AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 500),
+    );
   }
 
   @override
@@ -53,24 +64,88 @@ class _MapsPageState extends State<MapsPage> {
             Provider.of<MapInteractionContainer>(context, listen: false)
                 .selectedLocation = pos,
       ),
-      //create Branch for adv_fab
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Provider
-              .of<MapInteractionContainer>(context, listen: false)
-              .selectedLocation ??= _lastLocation;
-          logging.log(_lastLocation.toString());
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => ShowSelectionList()),
-          );
-        },
-        backgroundColor: Theme
-            .of(context)
-            .colorScheme
-            .primary,
-        child: const Icon(Icons.add),
+      floatingActionButton: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: getWidgetListForAdvFab()
+          ..add(
+            FloatingActionButton(
+              heroTag: null,
+              onPressed: () {
+                if (_controller.isDismissed) {
+                  _controller.forward();
+                } else {
+                  _controller.reverse();
+                }
+              },
+              child: AnimatedBuilder(
+                animation: _controller,
+                builder: (BuildContext context, Widget child) {
+                  return Transform(
+                    transform:
+                        Matrix4.rotationZ(_controller.value * 0.5 * math.pi),
+                    alignment: FractionalOffset.center,
+                    child: Icon(
+                        _controller.isDismissed ? Icons.add : Icons.close),
+                  );
+                },
+              ),
+            ),
+          ),
       ),
     );
+  }
+
+  List<Widget> getWidgetListForAdvFab(){    //TODO: discuss which icons should be used
+    List<Widget> result = [
+      Container(
+        height: 56.0,
+        width: 75.0,
+        alignment: FractionalOffset.centerLeft,
+        child: ScaleTransition(
+          scale: CurvedAnimation(
+            parent: _controller,
+            curve: Interval(0.0, 1.0 - 0 / icons.length / 2.0,
+                curve: Curves.easeOut),
+          ),
+          child: FloatingActionButton(
+            heroTag: null,
+            tooltip: 'Wunsch hinzufügen',
+            backgroundColor: Theme.of(context).cardColor,
+            onPressed: () {},
+            child: Icon(icons[0], color: Theme.of(context).accentColor),
+          ),
+        ),
+      ),
+      Container(
+        height: 56.0,
+        width: 75.0,
+        alignment: FractionalOffset.centerLeft,
+        child: ScaleTransition(
+          scale: CurvedAnimation(
+            parent: _controller,
+            curve: Interval(0.0, 1.0 - 1 / icons.length / 2.0,
+                curve: Curves.easeOut),
+          ),
+          child: FloatingActionButton(
+            heroTag: null,
+            tooltip: 'Element hinzufügen',
+            backgroundColor: Theme.of(context).cardColor,
+            onPressed: () {
+              Provider
+                  .of<MapInteractionContainer>(context, listen: false)
+                  .selectedLocation ??= _lastLocation;
+              logging.log(_lastLocation.toString());
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => ShowSelectionList()),
+              );
+            },
+            child: Icon(icons[1], color: Theme.of(context).accentColor),
+          ),
+        ),
+      ),
+    ];
+
+    return result;
   }
 }
