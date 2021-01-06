8656ba5765a343a28abb575ae304219787886819
Gabriel Ineichen <gabriel.ineichen@students.fhnw.ch>
2021-01-06 15:32:42 +0100

    basic tests

--- test/firebase/general_mock_test.dart
+++ test/firebase/general_mock_test.dart
@@ -1,9 +1,10 @@
 // import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter_test/flutter_test.dart';
 //import '../../lib/authentication/your_auth_using_firebase.dart';
 import 'package:biodiversity/main.dart';
 import 'package:biodiversity/models/user.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_test/flutter_test.dart';
+
 import './firebase_auth_mock.dart'; // from: https://github.com/FirebaseExtended/flutterfire/blob/master/packages/firebase_auth/firebase_auth/test/mock.dart
 // see more here: https://stackoverflow.com/questions/63662031/how-to-mock-the-firebaseapp-in-flutter
 
@@ -20,7 +21,10 @@ void main() {
     User testUser = User.empty();
     testUser.updateUserData(newName: "Manu");
     expect(testUser.name, "Manu");
-
+    expect(testUser.isLoggedIn(), false);
+    expect(testUser.doesLikeElement("plant"), false);
+    testUser.likeUnlikeElement("plant");
+    expect(testUser.doesLikeElement("plant"), true);
     // Tests to write
   });
 }
