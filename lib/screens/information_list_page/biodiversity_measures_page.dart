f1fcf9de66d52f0aeabab4856b3f3fc49166834b
ThierryOdermatt <68233005+ThierryOdermatt@users.noreply.github.com>
2021-02-24 21:11:27 +0100

    added non working searchbar to lists

--- lib/screens/information_list_page/biodiversity_measures_page.dart
+++ lib/screens/information_list_page/biodiversity_measures_page.dart
@@ -1,16 +1,43 @@
 import 'package:biodiversity/components/biodiversity_item_list_widget.dart';
 import 'package:biodiversity/components/drawer.dart';
+import 'package:biodiversity/fonts/icons_biodiversity_icons.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 //TODO rename to habitatelements
 class InformationListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: const Text("Lebensräume")),
-        drawer: MyDrawer(),
-        body: BiodiversityItemListWidget(
-          useSimpleCard: false,
-        ));
+      appBar: AppBar(title: const Text("Lebensräume")),
+      drawer: MyDrawer(),
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: new EdgeInsets.fromLTRB(8, 0, 8, 0),
+            child: Row(
+              children: <Widget>[
+                Icon(FontAwesomeIcons.search, size: 20),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(8.0, 0, 8.0, 0),
+                    child: TextField(
+                      decoration: const InputDecoration(
+                          labelText: 'Suchen'),
+                    ),
+                  ),
+                ),
+
+              ],
+            ),
+          ),
+          Expanded(
+            child: BiodiversityItemListWidget(
+              useSimpleCard: false,
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
--- lib/screens/species_list_page/species_list_page.dart
+++ lib/screens/species_list_page/species_list_page.dart
@@ -1,13 +1,35 @@
 import 'package:biodiversity/components/drawer.dart';
 import 'package:biodiversity/components/species_item_list_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class SpeciesListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: const Text("Arten")),
-        drawer: MyDrawer(),
-        body: SpeciesItemListWidget(useSimpleCard: false));
+      appBar: AppBar(title: const Text("Arten")),
+      drawer: MyDrawer(),
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: new EdgeInsets.fromLTRB(8, 0, 8, 0),
+            child: Row(
+              children: <Widget>[
+                Icon(FontAwesomeIcons.search, size: 20),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(8.0, 0, 8.0, 0),
+                    child: TextField(
+                      decoration: const InputDecoration(labelText: 'Suchen'),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+          Expanded(child: SpeciesItemListWidget(useSimpleCard: false)),
+        ],
+      ),
+    );
   }
 }
