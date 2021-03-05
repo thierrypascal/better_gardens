de5b8ae43d8a86aac9ce60acdcdecc2ffdcbd1fb
Gabriel Ineichen <gabriel.ineichen@students.fhnw.ch>
2021-03-05 15:00:57 +0100

    changed to markdown

--- lib/screens/detailview_page/detailview_page_measure.dart
+++ lib/screens/detailview_page/detailview_page_measure.dart
@@ -3,6 +3,7 @@ import 'package:biodiversity/models/biodiversity_measure.dart';
 import 'package:biodiversity/models/user.dart';
 import 'package:biodiversity/screens/information_list_page/biodiversity_measures_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:provider/provider.dart';
 
 class DetailViewPageMeasure extends StatefulWidget {
@@ -20,112 +21,99 @@ class _DetailViewPageMeasureState extends State<DetailViewPageMeasure> {
     return Scaffold(
       appBar: AppBar(title: Text("Details: ${widget.element.name}")),
       drawer: MyDrawer(),
-      body: LayoutBuilder(
-        builder: (context, constraint) {
-          return SingleChildScrollView(
-            child: ConstrainedBox(
-              constraints: BoxConstraints(
-                  minHeight: constraint.maxHeight,
-                  minWidth: constraint.maxWidth),
-              child: Column(
-                children: [
-                  Image(
-                    image: AssetImage(widget.element.imageSource),
-                    fit: BoxFit.fitWidth,
-                    height: 150,
-                    width: MediaQuery.of(context).size.width,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
+      body: Column(
+        children: [
+          Image(
+            image: AssetImage(widget.element.imageSource),
+            fit: BoxFit.fitWidth,
+            height: 150,
+            width: MediaQuery.of(context).size.width,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              FlatButton(
+                  child: Row(
                     children: [
-                      FlatButton(
-                          child: Row(
-                            children: [
-                              const Icon(Icons.arrow_back),
-                              SizedBox(width: 5),
-                              Text("Zurück zur Liste"),
-                            ],
-                          ),
-                          onPressed: () {
-                            Navigator.canPop(context)
-                                ? Navigator.pop(context)
-                                : Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (context) =>
-                                            InformationListPage()),
-                                  );
-                          }),
+                      const Icon(Icons.arrow_back),
+                      SizedBox(width: 5),
+                      Text("Zurück zur Liste"),
                     ],
                   ),
-                  Padding(
-                    padding: const EdgeInsets.all(20.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+                  onPressed: () {
+                    Navigator.canPop(context)
+                        ? Navigator.pop(context)
+                        : Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => InformationListPage()),
+                          );
+                  }),
+            ],
+          ),
+          Expanded(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              widget.element.name,
-                              style: const TextStyle(
-                                  fontWeight: FontWeight.bold, fontSize: 20),
-                            ),
-                            Row(children: [
-                              Consumer<User>(
-                                builder: (context, user, child) {
-                                  if (user == null) {
-                                    return const Text("");
-                                  }
-                                  return Row(
-                                    children: [
-                                      IconButton(
-                                        icon: Icon(
-                                          Icons.add,
-                                        ),
-                                        onPressed: () {},
-                                      ),
-                                      IconButton(
-                                        icon: user.doesLikeElement(
-                                                widget.element.name)
-                                            ? const Icon(Icons.favorite)
-                                            : const Icon(Icons.favorite_border),
-                                        color: user.doesLikeElement(
-                                                widget.element.name)
-                                            ? Colors.red
-                                            : Colors.black38,
-                                        onPressed: () {
-                                          setState(() {
-                                            user.likeUnlikeElement(
-                                                widget.element.name);
-                                          });
-                                        },
-                                      ),
-                                    ],
-                                  );
-                                },
-                              ),
-                            ])
-                          ],
+                        Text(
+                          widget.element.name,
+                          style: const TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 20),
                         ),
-                        //Markdown(data: "# Test"),
-                        const SizedBox(
-                          height: 20,
-                        ),
-                        const Text(
-                          "Gut für die folgenden Tiere:",
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold, fontSize: 15),
-                        ),
-                        Text(widget.element.beneficialFor),
+                        Consumer<User>(
+                          builder: (context, user, child) {
+                            return Row(
+                              children: [
+                                IconButton(
+                                  icon: Icon(
+                                    Icons.add,
+                                  ),
+                                  onPressed: () {},
+                                ),
+                                IconButton(
+                                  icon:
+                                      user.doesLikeElement(widget.element.name)
+                                          ? const Icon(Icons.favorite)
+                                          : const Icon(Icons.favorite_border),
+                                  color:
+                                      user.doesLikeElement(widget.element.name)
+                                          ? Colors.red
+                                          : Colors.black38,
+                                  onPressed: () {
+                                    setState(() {
+                                      user.likeUnlikeElement(
+                                          widget.element.name);
+                                    });
+                                  },
+                                ),
+                              ],
+                            );
+                          },
+                        )
                       ],
                     ),
-                  ),
-                ],
+                    MarkdownBody(data: widget.element.description),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    const Text(
+                      "Gut für die folgenden Tiere:",
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
+                    ),
+                    Text(widget.element.beneficialFor),
+                  ],
+                ),
               ),
             ),
-          );
-        },
+          ),
+        ],
       ),
     );
   }
